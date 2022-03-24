21ebd603bdffbc5d46860e928ab260cb3b08b4cb
Dimitrios Iliadis <jimmyhliad@gmail.com>
2022-03-24 08:35:06 +0000

    adding ranking visualizations

--- streamlit_interface.py
+++ streamlit_interface.py
@@ -8,27 +8,91 @@ from scipy.stats import rankdata
 import numpy as np
 
 
-def get_ranking(
-    hyperopt_runtime_arr_mean,
-    hyperopt_performance_arr_mean,
-    hyperopt_performance_arr_min,
-    hyperopt_performance_arr_max,
-    hyperopt_performance_arr_std,
-):
-    interpolated_per_HPO_method = {}
-    interpolator_per_HPO_method = {}
-    data_per_HPO_method = {}
+def get_list_of_settings(aggregate_option):
+    if aggregate_option == "Classification":
+        return ["MLC", "MTL", "DP"]
+    elif aggregate_option == "Regression":
+        return ["MTR", "MC"]
+    else:
+        return aggregate_option
 
-    data_per_HPO_method[hp_name] = [
-        hyperopt_runtime_arr_mean,
-        hyperopt_performance_arr_mean,
-        hyperopt_performance_arr_min,
-        hyperopt_performance_arr_max,
-        hyperopt_performance_arr_std,
-    ]
-    interpolator_per_HPO_method[hp_name] = interp1d(
-        hyperopt_runtime_arr_mean, hyperopt_performance_arr_mean, bounds_error=False
-    )
+
+def get_ranking(
+    base_path="streamlit_cached_data/",
+    dataset_option="bibtex",
+    metric_option="auroc",
+    averaging_option="macro",
+    resolution=None,
+):
+    HPO_names = ["hyperband", "random_search", "2_x_random_search", "SMAC", "BOHB"]
+
+    results_per_dataset = {}
+    for mode in ["val", "test"]:
+        interpolated_per_HPO_method = {}
+        interpolator_per_HPO_method = {}
+        data_per_HPO_method = {}
+        for hp_name in HPO_names:
+
+            temp_dir = (
+                base_path
+                + dataset_option
+                + "/"
+                + metric_option
+                + "/"
+                + averaging_option
+                + "/"
+                + hp_name
+                + "/"
+                + mode
+            )
+            temp_data_dict = get_data_to_plot(temp_dir)
+
+            data_per_HPO_method[hp_name] = [
+                temp_data_dict["hyperopt_runtime_arr_mean"],
+                temp_data_dict["hyperopt_performance_arr_mean"],
+                temp_data_dict["hyperopt_performance_arr_min"],
+                temp_data_dict["hyperopt_performance_arr_max"],
+                temp_data_dict["hyperopt_performance_arr_std"],
+            ]
+
+            interpolator_per_HPO_method[hp_name] = interp1d(
+                temp_data_dict["hyperopt_runtime_arr_mean"],
+                temp_data_dict["hyperopt_performance_arr_mean"],
+                bounds_error=False,
+                fill_value=(
+                    np.nan,
+                    temp_data_dict["hyperopt_performance_arr_mean"][-1],
+                ),
+            )
+
+        # calculate the min and max time points across all HPO methods
+        min_x = min([data[0][0] for HPO_name, data in data_per_HPO_method.items()])
+        max_x = max([data[0][-1] for HPO_name, data in data_per_HPO_method.items()])
+        if resolution is None:
+            resolution = int(max_x - min_x)
+            print("Calculating maximum resolution: " + str(resolution))
+        global_x = np.linspace(min_x, max_x, resolution)
+
+        for hp_name in HPO_names:
+            interpolated_per_HPO_method[hp_name] = interpolator_per_HPO_method[hp_name](
+                global_x
+            )
+
+        raw_ratings_arr = np.array([d for d in interpolated_per_HPO_method.values()])
+        if metric_option in ["RRMSE", "RMSE", "MSE", "MAE"]:
+            rankings_arr = rankdata(raw_ratings_arr, axis=0, method="min")
+        else:
+            rankings_arr = rankdata(-1 * raw_ratings_arr, axis=0, method="min")
+
+        rankings_arr = rankings_arr.astype(float)
+        rankings_arr[np.isnan(raw_ratings_arr)] = np.nan
+
+        results_per_dataset[mode] = {
+            "rankings": rankings_arr,
+            "raw_ratings": raw_ratings_arr,
+        }
+
+    return results_per_dataset
 
 
 @st.experimental_memo
@@ -272,9 +336,11 @@ dir_structure_dict = get_file_structure(base_path)
 
 st.sidebar.title("HPO benchmarks for DeepMTP")
 
-viz_option = st.sidebar.radio("Visualization mode:", ("Individual", "Aggregated"))
+viz_option = st.sidebar.radio(
+    "Visualization mode:", ("Individual performance plots", "Aggregated ranking plots")
+)
 
-if viz_option == "Individual":
+if viz_option == "Individual performance plots":
 
     # dataset selector
     dataset_option = st.sidebar.selectbox(
@@ -344,5 +410,144 @@ if viz_option == "Individual":
 
 
 else:
-    st.header("Will be implemented soon...")
+    base_path = "streamlit_cached_data/"
 
+    HPO_names = ["hyperband", "random_search", "2_x_random_search", "SMAC", "BOHB"]
+    info_per_dataset = [
+        {
+            "MTP_setting": "MLC",
+            "dataset_name": "bibtex",
+            "metric_name": "aupr",
+            "metric_average": "macro",
+        },
+        {
+            "MTP_setting": "MLC",
+            "dataset_name": "Corel5k",
+            "metric_name": "aupr",
+            "metric_average": "macro",
+        },
+        {
+            "MTP_setting": "MTR",
+            "dataset_name": "rf2",
+            "metric_name": "RRMSE",
+            "metric_average": "macro",
+        },
+        {
+            "MTP_setting": "MTR",
+            "dataset_name": "scm1d",
+            "metric_name": "RRMSE",
+            "metric_average": "macro",
+        },
+        {
+            "MTP_setting": "MTL",
+            "dataset_name": "dog",
+            "metric_name": "aupr",
+            "metric_average": "macro",
+        },
+        {
+            "MTP_setting": "MTL",
+            "dataset_name": "bird",
+            "metric_name": "aupr",
+            "metric_average": "macro",
+        },
+        {
+            "MTP_setting": "MC",
+            "dataset_name": "movielens_100k",
+            "metric_name": "RMSE",
+            "metric_average": "micro",
+        },
+        {
+            "MTP_setting": "MC",
+            "dataset_name": "movielens_1M",
+            "metric_name": "RMSE",
+            "metric_average": "micro",
+        },
+        {
+            "MTP_setting": "DP",
+            "dataset_name": "ern",
+            "metric_name": "aupr",
+            "metric_average": "micro",
+        },
+        {
+            "MTP_setting": "DP",
+            "dataset_name": "srn",
+            "metric_name": "aupr",
+            "metric_average": "micro",
+        },
+    ]
+
+    type_aggregation_option = st.sidebar.radio(
+        "Select a type of aggregation", ("Predefined", "Manual")
+    )
+    list_of_datasets = []
+    if type_aggregation_option == "Predefined":
+
+        # aggregate selector
+        aggregate_option = st.sidebar.selectbox(
+            "Select a aggregation mode",
+            ["Classification", "Regression", "MLC", "MTR", "MTL", "MC", "DP"],
+        )
+
+        list_of_settings = get_list_of_settings(aggregate_option)
+
+        list_of_datasets = [
+            d for d in info_per_dataset if d["MTP_setting"] in list_of_settings
+        ]
+
+    else:
+
+        dataset_options = st.multiselect(
+            "Select one or more of the datasets",
+            [d_info["dataset_name"] for d_info in info_per_dataset],
+            ["bibtex", "Corel5k"],
+        )
+
+        list_of_datasets = [
+            d for d in info_per_dataset if d["dataset_name"] in dataset_options
+        ]
+
+    if len(list_of_datasets) != 0:
+        result_per_dataset = []
+        # get the ranking matrices from every dataset-metric combo
+        for dataset in list_of_datasets:
+
+            result_per_dataset.append(
+                get_ranking(
+                    base_path=base_path,
+                    dataset_option=dataset["dataset_name"],
+                    metric_option=dataset["metric_name"],
+                    averaging_option=dataset["metric_average"],
+                    resolution=100,
+                )
+            )
+
+        # calculate the average ranking (separately for the validation and test sets)
+        for mode in ["val", "test"]:
+            # this is the idea of the padding option. It doesn't make sense as you lose temporal information. Sampling works better
+
+            ranking_per_dataset = np.array(
+                [d[mode]["rankings"] for d in result_per_dataset]
+            )
+            print("Averaging over " + str(len(ranking_per_dataset)) + " datasets")
+            average_ranking = np.nanmean(ranking_per_dataset, axis=0)
+            fig = go.Figure()
+            fig.update_layout(
+                xaxis=dict(title="% of runtime"), yaxis=dict(title="ranking"),
+            )
+
+            for i in range(average_ranking.shape[0]):
+                nan_ids = np.isnan(average_ranking[i, :])
+                fig.add_trace(
+                    go.Scatter(
+                        x=np.arange(average_ranking.shape[1])[~nan_ids],
+                        y=average_ranking[i, ~nan_ids],
+                        mode="lines+markers",
+                        name=HPO_names[i],
+                    )
+                )
+            st.title(mode)
+            st.plotly_chart(fig, use_container_width=True)
+            st.markdown("""---""")
+
+    else:
+        st.warning("No datasets or MTP settings selected")
